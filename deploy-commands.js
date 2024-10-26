08ec4040e1b2c89b5e55f4519ad03e4ba06a3422
Sonny J <60908891+iffyspeak-chroma@users.noreply.github.com>
2024-10-26 08:46:53 -0500

    Dont be stupid.

--- deploy-commands.js
+++ deploy-commands.js
@@ -32,16 +32,18 @@ const rest = new REST().setToken(token);
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        foreach (guildId in guildIds)
-        {
+        for (let index = 0; index < guildIds.length; index++)
+        { 
             // The put method is used to fully refresh all commands in the guild with the current set
             const data = await rest.put(
-                Routes.applicationGuildCommands(clientId, guildId),
-                { body: commands },
+                Routes.applicationGuildCommands(clientId, guildIds[index]),
+                   { body: commands },
             );
+
+            console.log(`Successfully reloaded ${data.length} application (/) commands in guild ${guildIds[index]}`);
         }
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
