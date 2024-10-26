d68b9e6b6cc07d22b316dfdfc84a88d99ab5ba4f
Sonny J <60908891+iffyspeak-chroma@users.noreply.github.com>
2024-10-26 07:47:13 -0500

    Kinda just set some stuff up

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("test :)");
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { TOKEN } = process.env.TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, cli => {
+    console.log(`Logged in as ${cli.user.tag}`);
+});
